737e3a630f9e53016c39cf1a26e43807551c4947
admin <hernandezgarciadaniel837@gmail.com>
2023-10-16 11:04:09 -0400

    version 1

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Custom Invoice Report',
+    'version': '1.0',
+    'summary': 'Add signature fields to the billing and accounting report',
+    'author': 'daniherzdeveloper',
+    'depends': ['account'],
+    'data': [
+        'report/custom_report_inherit.xml',
+    ],
+}
